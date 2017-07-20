5c517817f47d0eaded4efa66de3d78ad1caacd63
name <email@email.com>
2017-07-20 16:06:16 +0800

    change dir stuc

---
+++ samples/MultipartUpload.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/Common.php';
+
+use xing393939\Hello;
+Hello::fn();
+
+
+
---
+++ src/xing393939/Hello.php
@@ -0,0 +1,9 @@
+<?php namespace xing393939;
+
+class Hello
+{
+    public static function fn()
+    {
+        echo 'hello: ' . __METHOD__ . "\n";
+    }
+}
