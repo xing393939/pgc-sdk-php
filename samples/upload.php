a5423126f199e992b15f7b1c3d0c92cf2d3139d5
name <email@email.com>
2017-07-21 17:38:52 +0800

    upload muti file

--- samples/upload.php
+++ samples/upload.php
@@ -5,5 +5,5 @@ use xing393939\PgcSDK;
 
 $client = new PgcSDK('scdddk@163.com', 'FBBA0D39FA8F0A34814B1DDC24FCBB42');
 
-var_dump($client->upload(__DIR__ . '/example.mp4', 1));
+var_dump($client->upload('C:\\Esko\\bg_data_system_v010\\3.mp4', '', 'title', 'summary'));
 
--- src/xing393939/pgcSDK.php
+++ src/xing393939/pgcSDK.php
@@ -9,6 +9,7 @@ class PgcSDK
 
     private $username;
     private $apiKey;
+    private $errorMsg;
 
     public function __construct($username, $apiKey)
     {
@@ -16,9 +17,9 @@ class PgcSDK
         $this->apiKey = $apiKey;
     }
 
-    public function upload($filePath, $suffix)
+    public function upload($filePath, $categoryId, $name, $summary)
     {
-        $ppFeature = $this->getPpFeature($filePath);
+        $ppFeature = Unit::getPpFeature($filePath);
 
         //获取uptoken
         $url = self::OPEN_PLATFORM_HOST . '/token/uptoken';
@@ -27,44 +28,89 @@ class PgcSDK
             'ppfeature' => $ppFeature,
             'apitk' => md5($this->apiKey . $url),
         );
-        $arr = json_decode(Unit::send_get($url . '?' . http_build_query($params)), 1);
-
-        return $arr;
+        $tokenRs = json_decode(Unit::send_get($url . '?' . http_build_query($params)), 1);
 
         //获取fid
+        $url = self::OPEN_PLATFORM_HOST . '/channel/upload';
+        $postData = array(
+            'username' => $this->username,
+            'apitk' => md5($this->apiKey . $url),
+            'categoryid' => $categoryId,
+            'name' => urlencode($name),
+            'summary' => urlencode($summary),
+            'coverimg' => '',
+            'length' => filesize($filePath),
+            'ppfeature' => $ppFeature,
+        );
+        $fidRs = json_decode(Unit::send_json_post($url, $postData), 1);
 
-        //取uploadRange
+        //获取上传信息信息
+        $url = self::PPC_HOST . "/{$fidRs['data']['fId']}/uploading?fromcp=private_cloud";
+        $header = [
+            "Authorization: {$tokenRs['data']}",
+            'Accept: application/json',
+        ];
+        $uploadingRs = json_decode(Unit::send_get($url, $header), 1);
 
-        //上传片段
+        //上传md5
+        $md5 = strtoupper(md5_file($filePath));
+        $url = self::PPC_HOST . "/{$fidRs['data']['fId']}/md5?fromcp=private_cloud&feature_pplive={$ppFeature}&md5={$md5}";
+        $header = [
+            "Authorization: {$tokenRs['data']}",
+            'Accept: application/json',
+        ];
+        $md5Rs = json_decode(Unit::send_json_post($url, $postData, $header), 1);
+
+        //uploadRange
+        $rangeRs = $this->uploadRange($filePath, $ppFeature, $fidRs['data']['fId'], $tokenRs['data']);
 
         //完成
-
+        return [$tokenRs, $fidRs, $uploadingRs, $md5Rs, $rangeRs];
     }
 
-    public function getUploadInfoByFilePath($filePath)
-    {
+    private function uploadRange($filePath, $ppFeature, $fid, $token) {
+        $url = self::PPC_HOST . "/{$fid}/action/uploadrange?fromcp=private_cloud&feature_pplive={$ppFeature}&segs=1&inner=false";
+        $header = [
+            "Authorization: {$token}",
+            'Accept: application/json',
+        ];
+        $rangeRs = json_decode(Unit::send_get($url, $header), 1);
+        if (!isset($rangeRs['err']) || $rangeRs['err'] != 0) {
+            $this->errorMsg = 'PPC_HOST action/uploadrange api error';
+            return false;
+        }
+        //分段没有传完继续传
+        if ($rangeRs['data']['fileSize'] <= $rangeRs['data']['fileSize']) {
+            if (empty($rangeRs['data']['ranges']) || empty($rangeRs['data']['ranges'][0])) {
+                $this->errorMsg = 'PPC_HOST action/uploadrange api error';
+                return false;
+            }
 
+            //传分片
+            $range = $rangeRs['data']['ranges'][0];
+            $fp = fopen($filePath, 'r');
+            fseek($fp, $range['start']);
+            $str = fread($fp, $range['end'] - $range['start']);
+            fclose($fp);
+            $uploadId = md5($str);
+            $header = [
+                "Authorization: {$token}",
+                'Accept: */*',
+                'Etag: ' . md5($str)
+            ];
+            $uploadStr = Unit::send_put_file($range['upload_url'], $str, $header);
+
+            //传分片成功
+            $md5 = strtoupper($uploadId);
+            $url = self::PPC_HOST . "/{$fid}/action/uploaded?fromcp=private_cloud&range_md5={$md5}&bid={$range['bid']}&uploadid={$uploadId}";
+            $header = [
+                "Authorization: {$token}",
+                'Accept: application/json',
+            ];
+            $uploadedRs = json_decode(Unit::send_json_post($url, array(), $header), 1);
+
+            //$this->uploadRange($filePath, $ppFeature, $fid, $token);
+        }
+        return $uploadedRs;
     }
-
-    public function getPpFeature($filePath)
-    {
-        $size = filesize($filePath);
-        $length = 12288;
-        $str = '';
-        $fp = fopen($filePath, 'r');
-        fseek($fp, 0);
-        $str .= fread($fp, $length);
-        fseek($fp, $size / 5);
-        $str .= fread($fp, $length);
-        fseek($fp, 2 * $size / 5);
-        $str .= fread($fp, $length);
-        fseek($fp, 3 * $size / 5);
-        $str .= fread($fp, $length);
-        fseek($fp, $size - $length);
-        $str .= fread($fp, $length);
-        fclose($fp);
-
-        return $size . '_' . sha1($str);
-    }
-
 }
